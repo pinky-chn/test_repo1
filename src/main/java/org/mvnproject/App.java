2361b6ce67b321c59fb7f69e31b80382a1334d49
pinkiee <pinkychn89@gmail.com>
2014-09-13 18:47:45 -0500

    Checking in changes for branch 1

--- src/main/java/org/mvnproject/App.java
+++ src/main/java/org/mvnproject/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello to Git!" );
+	System.out.println( "Additional code for branch 1!" );
     }
 }
